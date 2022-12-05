87a16e29655bff379c14407b3a8069ae3e19906c
nqZyx <nqzyx@nqzyx.xyz>
2022-12-05 07:26:42 -0700

    Add ESLint

--- 2022/Day 01 - Calorie Counting/part1.js
+++ 2022/Day 01 - Calorie Counting/part1.js
@@ -1,18 +1,19 @@
-const { readFileSync } = require('fs');
+const { readFileSync } = require('fs')
 
-console.log(readFileSync('./input.txt', 'utf-8')
-  .split(/\r?\n\r?\n/)
-  .map((inventoryAsString) =>
-    inventoryAsString
-      .split('\n')
-      .reduce(
-        (totalCalories, caloriesAsString) =>
-          (totalCalories += parseInt(caloriesAsString)),
-        0
-      )
-  )
-  .sort((e1, e2) => e2 - e1)
-  [0]);
+console.log(
+  readFileSync('./input.txt', 'utf-8')
+    .split(/\r?\n\r?\n/)
+    .map((inventoryAsString) =>
+      inventoryAsString
+        .split('\n')
+        .reduce(
+          (totalCalories, caloriesAsString) =>
+            (totalCalories += parseInt(caloriesAsString)),
+          0
+        )
+    )
+    .sort((e1, e2) => e2 - e1)[0]
+)
 
 // console.log(`There are ${caloriesPerElf.length} elves.`);
 // console.log(`The highest calorie count for one elf is ${caloriesPerElf[0]}.`);
--- 2022/Day 01 - Calorie Counting/part2.js
+++ 2022/Day 01 - Calorie Counting/part2.js
@@ -1,17 +1,18 @@
 const { readFileSync } = require('fs')
 
-console.log(readFileSync('./input.txt', 'utf-8')
-  .split(/\r?\n\r?\n/)
-  .map((inventoryAsString) =>
-    inventoryAsString
-      .split('\n')
-      .reduce(
-        (totalCalories, caloriesAsString) =>
-          (totalCalories += parseInt(caloriesAsString)),
-        0
-      )
-  )
-  .sort((e1, e2) => e2 - e1)
-  .slice(0,3)
-  .reduce((totalCalories, calories) => (totalCalories += calories), 0)
-);
+console.log(
+  readFileSync('./input.txt', 'utf-8')
+    .split(/\r?\n\r?\n/)
+    .map((inventoryAsString) =>
+      inventoryAsString
+        .split('\n')
+        .reduce(
+          (totalCalories, caloriesAsString) =>
+            (totalCalories += parseInt(caloriesAsString)),
+          0
+        )
+    )
+    .sort((e1, e2) => e2 - e1)
+    .slice(0, 3)
+    .reduce((totalCalories, calories) => (totalCalories += calories), 0)
+)
