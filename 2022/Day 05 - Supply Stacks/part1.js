a43814454add1590203bfe26ec4d651fdedfa7e2
nqZyx <nqzyx@nqzyx.xyz>
2022-12-04 23:27:39 -0700

    Add 2022/Day 05...

---
+++ 2022/Day 05 - Supply Stacks/part1.js
@@ -0,0 +1,31 @@
+const { readFileSync } = require('fs')
+
+const stacks = []
+
+const [initialStacks, moves] = readFileSync('input.txt', 'utf8').split(
+  '\n\n'
+)
+
+initialStacks
+  .split('\n')
+  .reverse()
+  .map((level) => {
+    for (let s = 0; s < (level.length + 1) / 4; s++) {
+      const crate = level.slice(s * 4, s * 4 + 4)
+      if (crate.match(/^\[[A-Z]\]/)) {
+        if (!(stacks[s] instanceof Array)) stacks[s] = []
+        stacks[s].push(crate.slice(1, 2))
+      }
+    }
+    return level
+  })
+
+moves
+  .split('\n')
+  .map((move) => {
+    const [ _, boxes, from, to ] = move.match(/^move (\d+) from (\d+) to (\d+)$/)
+    stacks[to - 1].push(...stacks[from - 1].splice(-boxes).reverse())
+    return move
+  })
+
+console.log(stacks.map((stack) => stack.slice(-1)[0]).join(''))
