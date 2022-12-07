978e3b999b38b721f22f16be8dad5c71a0e31fac
nqZyx <nqzyx@nqzyx.xyz>
2022-12-07 05:00:01 -0700

    More 2015/Day 07... WIP

--- 2015/Day 07 - Some Assembly Required/part1.js
+++ 2015/Day 07 - Some Assembly Required/part1.js
@@ -1,114 +1,96 @@
 const { readFileSync } = require('fs')
 
-var circuit = readFileSync('input.txt', 'utf8')
+let wires = readFileSync('input.txt', 'utf8')
   .split('\n')
-  .map((spec) => {
-    const [input, wire] = spec.split(' -> ')
+  .reduce((updatedWires, connection) => {
+    const [input, wire] = connection.split(' -> ')
     if (input.match(/^\d+$/)) {
-      // Input: Value
-      return {
-        type: 'wire',
-        name: wire,
-        input: { type: 'value', value: parseInt(input) },
-      }
+      // Input is a constant
+      const value = parseInt(input)
+      updatedWires[wire] = { input: { type: 'value', value }, output: value }
     } else if (input.match(/^[a-z]+$/)) {
-      // Input: Wire
-      return {
-        type: 'wire',
-        name: wire,
-        input: { type: 'wire', name: input },
-      }
+      // Input is a wire
+      updatedWires[wire] = { input: { type: 'wire', name: input }, output: null }
     } else if (input.match(/^NOT ([a-z]+)$/)) {
-      // Input: NOT Gate
-      const [op, wire] = input.split(' ')
-      return {
-        type: 'wire',
-        name: wire,
-        input: {
-          type: 'gate',
-          name: op.toLowerCase(),
-          input: { type: 'wire', name: wire },
-        },
-      }
-    } else {
-      // Input: Gate
+      // Input is a NOT gate
+      const [op, inputWire] = input.split(' ')
+      updatedWires[wire] = { input: { type: 'gate', name: op.toLowerCase(), inputs: [{ type: 'wire', name: inputWire }] }, output: null }
+    } else if (input.match(/^([a-z]+) (AND|OR|LSHIFT|RSHIFT) ([a-z]+)$/)) {
+      // Input is a gate
       let [left, op, right] = input.split(' ')
-      left = left.match(/^\d+$/)
-        ? { type: 'value', value: parseInt(left) }
-        : left.match(/^[a-z]+$/)
-        ? { type: 'wire', name: left }
-        : `Unknown left input: ${left}`
-      right = right.match(/^\d+$/)
-        ? { type: 'value', value: parseInt(right) }
-        : right.match(/^[a-z]+$/)
-        ? { type: 'wire', name: right }
-        : `Unknown right input: ${right}`
-      return {
-        type: 'wire',
-        name: wire,
-        input: {
-          type: 'gate',
-          name: op.toLowerCase(),
-          input: { type: 'pair', left, right },
-        },
-      }
+      op = op.toLowerCase()
+      left = left.match(/^\d+$/) ? parseInt(left) : { type: 'wire', name: left }
+      right = right.match(/^\d+$/) ? parseInt(right) : { type: 'wire', name: right }
+      updatedWires[wire] = { input: { type: 'gate', name: op, inputs: [left, right] }, output: null }
     }
-  })
+    return updatedWires
+  }, {})
 
-console.log(circuit)
+function getWireValue (name) {
+  const wire = wires[name]
+  return wire?.output ?? null
+}
 
-let wireValueUpdated = false
+let valuesUpdated = false
 do {
-  newCircuit = circuit.map((wire) => {
-    if (Object.keys(wire).includes('value') && wire.value) {
-      return wire;
-    }
-    if (wire.input.type === 'value') {
-      wire.value = wire.input.value
-      wireValueUpdated = true
-    } else if (wire.input.type === 'wire') {
-      const inputWire = circuit.find((w) => w.name === wire.input.name)
-      if (inputWire?.value) {
-        wire.value = inputWire.value
-        wireValueUpdated = true
-      }
-    } else if (wire.input.type === 'gate' && wire.input.name === 'not') {
-      const inputWire = circuit.find((w) => w.name === wire.input.input.name)
-      if (inputWire?.value) {
-        wire.value = ~inputWire.value & 0xffff
-        wireValueUpdated = true
-      }
-    } else if (wire.input.type === 'gate') {
-      const { name, input } = wire.input
-      if (input.type === 'pair') {
-        const { left, right } = input
-        if (left.type === 'wire' && right.type === 'wire') {
-          const leftWire = circuit.find((w) => w.name === left.name)
-          const rightWire = circuit.find((w) => w.name === right.name)
-          if (leftWire?.value && rightWire?.value) {
-            wire.value =
-              name === 'and'
-                ? leftWire.value & rightWire.value
-                : name === 'or'
-                ? leftWire.value | rightWire.value
-                : name === 'lshift'
-                ? leftWire.value << rightWire.value
-                : name === 'rshift'
-                ? leftWire.value >> rightWire.value
-                : `Unknown gate: ${name}`
-            wireValueUpdated = true
-          }
-        }
-      } else {
-        wire.value = `Unknown input: ${wire.input}`
-      }
-    }
-    return wire;
-  })
-  
-  if (wireValueUpdated) {
-    circuit = newCircuit
-  }
-} while (wireValueUpdated)
+  wires = Object.keys(wires).reduce((updatedWires, wireName) => {
+    valuesUpdated = false
+    const wire = wires[wireName]
+    const input = wire.input
 
-console.log(circuit);
+    console.log(`${wireName}: ${JSON.stringify(wire)}`)
+
+    switch (input.type) {
+      case 'value':
+        break
+
+      case 'wire':
+        // eslint-disable-next-line no-case-declarations
+        const value = getWireValue(input.name)
+        if (value instanceof Number) {
+          Object.assign(wire, { input: { type: 'value', value }, output: value })
+          valuesUpdated = true
+        }
+        break
+
+      case 'gate':
+        switch (input.name) {
+          case 'not':
+            // eslint-disable-next-line no-case-declarations
+            const value = getWireValue(input.inputs[0].name)
+            if (value instanceof Number) {
+              Object.assign(wire, { input: { type: 'value', value: ~value & 0xffff }, output: ~value & 0xffff })
+              valuesUpdated = true
+            }
+            break
+
+          default: // and || or || lshift || rshift
+            input.inputs.forEach((inputWire) => {
+              const inputWireValue = getWireValue(inputWire.name)
+              if (inputWireValue instanceof Number) {
+                Object.assign(inputWire, { input: { type: 'value', value: inputWireValue }, output: inputWireValue })
+                valuesUpdated = true
+              }
+            })
+            if (input.inputs.every((inputWire) => inputWire.output instanceof Number)) {
+              const [left, right] = input.inputs
+              const value = input.name === 'and'
+                ? left.output & right.output
+                : input.name === 'or'
+                  ? left.output | right.output
+                  : input.name === 'lshift'
+                    ? left.output << right.output
+                    : left.output >> right.output
+              Object.assign(wire, { input: { type: 'value', value }, output: value })
+              valuesUpdated = true
+            }
+            break
+        }
+        break
+    }
+    updatedWires[wireName] = wire
+    return updatedWires
+  }, {})
+} while (valuesUpdated)
+
+console.log(`Wires length: ${wires.length}`)
