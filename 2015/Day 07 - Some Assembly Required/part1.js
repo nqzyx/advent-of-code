3f257cdb1fdecc3580af9dd9a57633e15cb1176c
nqZyx <nqzyx@nqzyx.xyz>
2022-12-07 13:10:34 -0700

    Add 2015/Day 07... Part1 complete

--- 2015/Day 07 - Some Assembly Required/part1.js
+++ 2015/Day 07 - Some Assembly Required/part1.js
@@ -1,96 +1,57 @@
 const { readFileSync } = require('fs')
 
-let wires = readFileSync('input.txt', 'utf8')
-  .split('\n')
-  .reduce((updatedWires, connection) => {
-    const [input, wire] = connection.split(' -> ')
-    if (input.match(/^\d+$/)) {
-      // Input is a constant
-      const value = parseInt(input)
-      updatedWires[wire] = { input: { type: 'value', value }, output: value }
-    } else if (input.match(/^[a-z]+$/)) {
-      // Input is a wire
-      updatedWires[wire] = { input: { type: 'wire', name: input }, output: null }
-    } else if (input.match(/^NOT ([a-z]+)$/)) {
-      // Input is a NOT gate
-      const [op, inputWire] = input.split(' ')
-      updatedWires[wire] = { input: { type: 'gate', name: op.toLowerCase(), inputs: [{ type: 'wire', name: inputWire }] }, output: null }
-    } else if (input.match(/^([a-z]+) (AND|OR|LSHIFT|RSHIFT) ([a-z]+)$/)) {
-      // Input is a gate
-      let [left, op, right] = input.split(' ')
-      op = op.toLowerCase()
-      left = left.match(/^\d+$/) ? parseInt(left) : { type: 'wire', name: left }
-      right = right.match(/^\d+$/) ? parseInt(right) : { type: 'wire', name: right }
-      updatedWires[wire] = { input: { type: 'gate', name: op, inputs: [left, right] }, output: null }
-    }
-    return updatedWires
-  }, {})
-
-function getWireValue (name) {
-  const wire = wires[name]
-  return wire?.output ?? null
+function wireCheck (w) {
+  return `('${w}' in solvedWires)`
 }
 
-let valuesUpdated = false
-do {
-  wires = Object.keys(wires).reduce((updatedWires, wireName) => {
-    valuesUpdated = false
-    const wire = wires[wireName]
-    const input = wire.input
+function wireValue (w) {
+  return `solvedWires['${w}']`
+}
 
-    console.log(`${wireName}: ${JSON.stringify(wire)}`)
+const ops = { AND: '&', OR: '|', LSHIFT: '<<', RSHIFT: '>>' }
 
-    switch (input.type) {
-      case 'value':
-        break
+const solvedWires = {}
+let unsolvedWires = {}
 
-      case 'wire':
-        // eslint-disable-next-line no-case-declarations
-        const value = getWireValue(input.name)
-        if (value instanceof Number) {
-          Object.assign(wire, { input: { type: 'value', value }, output: value })
-          valuesUpdated = true
-        }
-        break
-
-      case 'gate':
-        switch (input.name) {
-          case 'not':
-            // eslint-disable-next-line no-case-declarations
-            const value = getWireValue(input.inputs[0].name)
-            if (value instanceof Number) {
-              Object.assign(wire, { input: { type: 'value', value: ~value & 0xffff }, output: ~value & 0xffff })
-              valuesUpdated = true
-            }
-            break
-
-          default: // and || or || lshift || rshift
-            input.inputs.forEach((inputWire) => {
-              const inputWireValue = getWireValue(inputWire.name)
-              if (inputWireValue instanceof Number) {
-                Object.assign(inputWire, { input: { type: 'value', value: inputWireValue }, output: inputWireValue })
-                valuesUpdated = true
-              }
-            })
-            if (input.inputs.every((inputWire) => inputWire.output instanceof Number)) {
-              const [left, right] = input.inputs
-              const value = input.name === 'and'
-                ? left.output & right.output
-                : input.name === 'or'
-                  ? left.output | right.output
-                  : input.name === 'lshift'
-                    ? left.output << right.output
-                    : left.output >> right.output
-              Object.assign(wire, { input: { type: 'value', value }, output: value })
-              valuesUpdated = true
-            }
-            break
-        }
-        break
+readFileSync('input.txt', 'utf8')
+  .split('\n')
+  .forEach((connection) => {
+    const [src, w] = connection.split(' -> ')
+    if (src.match(/^\d+$/)) { // Input is a constant
+      solvedWires[w] = parseInt(src)
     }
-    updatedWires[wireName] = wire
-    return updatedWires
-  }, {})
-} while (valuesUpdated)
+    if (src.match(/^[a-z]+$/)) { // Input is another wire
+      unsolvedWires[w] = `!${wireCheck(src)} ? undefined : ${wireValue(src)}`
+    }
+    if (src.match(/^NOT ([a-z]+)$/)) { // Input is a NOT gate
+      const [, iw] = src.split(' ')
+      unsolvedWires[w] = `!${wireCheck(iw)} ? undefined : (~${wireValue(iw)} & 0xffff)`
+    }
+    if (src.match(/ (AND|OR|LSHIFT|RSHIFT) /)) { // Input is an AND gate
+      const [l, o, r] = src.match(/^(\d+|[a-z]+) ([A-Z]+) (\d+|[a-z]+)$/).slice(1, 4)
+      const op = ops[o]
+      const lCheck = (l.match(/^[a-z]+$/)) ? wireCheck(l) : 'true'
+      const rCheck = (r.match(/^[a-z]+$/)) ? wireCheck(r) : 'true'
+      const lValue = (l.match(/^[a-z]+$/)) ? wireValue(l) : l
+      const rValue = (r.match(/^[a-z]+$/)) ? wireValue(r) : r
+      unsolvedWires[w] = `!(${lCheck} && ${rCheck}) ? undefined : (${lValue} ${op} ${rValue})`
+    }
+  })
 
-console.log(`Wires length: ${wires.length}`)
+while (Object.keys(unsolvedWires).length > 0) {
+  const latestUnsolvedWires = Object.keys(unsolvedWires)
+    .reduce((u, w) => {
+      // eslint-disable-next-line no-eval
+      const evalResult = eval(unsolvedWires[w])
+      if (evalResult === undefined) {
+        u[w] = unsolvedWires[w]
+      } else {
+        solvedWires[w] = evalResult
+      }
+      return u
+    }, {})
+
+  unsolvedWires = latestUnsolvedWires
+}
+
+console.log(solvedWires.a)
