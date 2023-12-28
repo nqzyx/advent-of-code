e8fa9def26382131670de3c06c728456d7ca4e5b
nqZyx <nqzyx@nqzyx.xyz>
2023-12-28 13:56:39 +0000

    Bug fix update go.work
    Signed-off-by: nqZyx <nqzyx@nqzyx.xyz>

--- 2023/internal/utils/arrays.go
+++ 2023/internal/utils/arrays.go
@@ -18,7 +18,7 @@ func NewNumericArrayFromString[T Numeric](str string) (result []T) {
 func NewNumericArrayFromStringWithSeparator[T Numeric](str string, sep string) (result []T) {
 	sArr := strings.Split(strings.TrimSpace(str), sep)
 	result = make([]T, 0, len(sArr))
-	for i, s := range sArr {
+	for _, s := range sArr {
 		var x T
 		if l, err := fmt.Sscanf(s, "%v", &x); err != nil {
 			panic(err)
@@ -26,7 +26,7 @@ func NewNumericArrayFromStringWithSeparator[T Numeric](str string, sep string) (
 			if l == 0 {
 				return
 			}
-			result[i] = T(x)
+			result = append(result, T(x))
 		}
 	}
 	return
